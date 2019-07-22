94b52a4351f5a1cc4e3c669b9c68c34013ed63a4
chazool <chazool@users.noreply.github.com>
2019-07-22 11:45:55 +0530

    menu login button action

--- app/src/main/java/com/example/mobile/MainActivity.java
+++ app/src/main/java/com/example/mobile/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.mobile;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.core.view.GravityCompat;
@@ -21,6 +23,9 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+
+
 
 import android.view.Menu;
 
@@ -60,20 +65,6 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
